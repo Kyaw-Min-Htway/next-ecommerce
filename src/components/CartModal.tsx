8f02d2a8aaa880c1ebd525269c8441ddb30f7f94
Kyaw Min Htway <batman066813@gmail.com>
2024-10-27 21:28:16 +0630

    categorylist

--- src/components/CartModal.tsx
+++ src/components/CartModal.tsx
@@ -11,6 +11,7 @@ const CartModal = () => {
                 <div className="">Cart is Empty</div>
             ) : (
                 <>
+                <h2 className="text-xl">Shopping Cart</h2>
                <div className="flex flex-col gap-8">
                 {/* ITEM */}
                 <div className="flex gap-4">
@@ -63,6 +64,13 @@ const CartModal = () => {
                 <div className="flex items-center justify-between font-semibold">
                     <span className="">Subtotal</span>
                     <span className="">$49</span>
+                </div>
+                <p className="text-gray-500 text-sm mt-2 mb-4">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque, eum!
+                </p>
+                <div className="flex justify-between text-sm">
+                    <button className="rounded-md py-3 px-4 ring-1 ring-gray-300">View Cart</button>
+                    <button className="rounded-md py-3 px-4 bg-black text-white">Checkout</button>
                 </div>
                </div>
                </>
